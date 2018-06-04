d57a5f1c3a6d73a1ef9797d8e80748b7a3c92b70
Maxwell Fischer <19.maxwell.fischer@ksd.org>
2018-06-04 13:55:53 -0700

    Forest level

---
+++ 3dteststuff/Assets/Scenes/Portals/Portals.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portals : MonoBehaviour {
+
+    public GameObject red;
+    public GameObject blue;
+    public bool isRed;
+    public Vector3 offset;
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Vector3 pos;
+        if (isRed)
+        {
+            pos = blue.transform.position;
+        }
+        else
+        {
+            pos = red.transform.position;
+        }
+        pos = new Vector3(pos.x + offset.x, pos.y + offset.y, pos.z + offset.z);
+        other.transform.SetPositionAndRotation(pos, Quaternion.identity);
+    }
+
+
+
+
+}
